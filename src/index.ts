199d4b01dd87b2a7f194a26f43d7ddf4011fbd6a
SHA Miao <shamiao@outlook.com>
2024-05-14 20:01:28 +0800

    temp draft, serves one toolchain

--- src/index.ts
+++ src/index.ts
@@ -11,8 +11,19 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+import { registry } from "./registry";
+
+async function fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+	const url = new URL(request.url);
+	if (request.method == 'GET') {
+		if (url.pathname == '/ubuntu_amd64/') {
+			return await registry(request, env, ctx);
+		}
+	}
+
+	return new Response("not found", { status: 404 });
+}
+
 export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
-	},
+	fetch	
 };
